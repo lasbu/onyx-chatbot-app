752cb263b8da590bcaae6ad41ded4a74e5424132
Lasse <lasse.bjolgerud@onyxcentersource.com>
2023-11-08 11:02:24 +0100

    Scroll to bottom when recieving new message. Support conversations

--- src/app/chatbot/chatbot.component.ts
+++ src/app/chatbot/chatbot.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { OpenAiApiService } from '../services/open-ai-api.service';
+import { ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-chatbot',
@@ -11,8 +12,12 @@ export class ChatbotComponent {
   assistantReply!: string;
   chatMessages: { role: string; content: string }[] = [];
   isLoading!: boolean;
+  
+  @ViewChild('chatContainer') private chatContainer!: ElementRef;
 
   constructor(private openAiApiService: OpenAiApiService) {}
+
+  
 
   sendMessage() {
     this.isLoading = true;
@@ -28,6 +33,35 @@ export class ChatbotComponent {
           content: this.assistantReply,
         });
         this.userMessage = '';
+        this.scrollToBottom();
       });
+  }
+
+  sendMessages() {
+    this.isLoading = true;
+    const userMessage = this.userMessage;
+    this.chatMessages.push({ role: 'user', content: userMessage });
+    // Create a string of all the messages separated by a newline
+    // and add a newline at the end
+    var cm = this.chatMessages.map((message) => message.content).join('\n') + '\n';
+    
+    this.openAiApiService
+      .sendMessage(cm)
+      .subscribe((response) => {
+        this.isLoading = false;
+        this.assistantReply = response.reply;
+        this.chatMessages.push({
+          role: 'assistant',
+          content: this.assistantReply,
+        });
+        this.userMessage = '';
+        this.scrollToBottom();
+      });
+  }
+
+  private scrollToBottom(): void {
+    setTimeout(() => {
+      this.chatContainer.nativeElement.scrollTop = this.chatContainer.nativeElement.scrollHeight;
+    }, 100);
   }
 }
