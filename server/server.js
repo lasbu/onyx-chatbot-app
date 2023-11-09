73304e749265c7769d139da76a408e61c3c53289
Fabian Medellin <fabian.medellin@onyxcentersource.com>
2023-11-09 09:14:56 -0600

    live demo with fake responses

--- server/server.js
+++ server/server.js
@@ -2,19 +2,20 @@ require('dotenv').config();
 const express = require("express");
 const { Configuration, OpenAIApi } = require("openai");
 const cors = require("cors");
+const { query } = require('express');
 
 const app = express();
 const port = 3000;
 
 const configuration = new Configuration({
-  apiKey: "YOUR_API_KEY",
+  apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
 app.use(express.json());
 app.use(cors());
 
-app.post("/chat", async (req, res) => {
+app.post("/chat2", async (req, res) => {
   const { message } = req.body;
 
   const completion = await openai.createChatCompletion({
@@ -26,7 +27,7 @@ app.post("/chat", async (req, res) => {
   res.json({ reply });
 });
 
-app.post("/chat-TEST", async (req, res) => {
+app.post("/chat", async (req, res) => {
   const { message } = req.body;
 
   // const completion = await openai.createChatCompletion({
@@ -35,8 +36,29 @@ app.post("/chat-TEST", async (req, res) => {
   // });
 
   // const reply = completion.data.choices[0].message.content;
-  reply = "Hello! How can I assist you today?"
-  await sleep(2000);
+  reply = "The service is not available. Try again later"
+  let query = String(message).toLowerCase();
+  reply = query
+  if(query.includes('hi') || query.includes('hello')){
+    reply = "Hello! How can I assist you today?"
+    await sleep(2000);
+  }
+  if(query.includes('march')){
+    reply = "5 events occurred in March 2023."
+    await sleep(4000);
+  }
+  if(query.includes('most popular')){
+    reply = "The most popular city for events in 2023 is Dallas."
+    await sleep(3000);
+  }
+  if(query.includes('many properties')){
+    reply = "7 properties are subscribed."
+    await sleep(4000);
+  }
+  if(query.includes('many event payments')){
+    reply = "176 event payments are currently rejected."
+    await sleep(3000);
+  }
   res.json({ reply });
 });
 app.listen(port, () => {
